922f257a951fdd74893a460670df895e3b6b4320
Rafael Neto <rafael.neto@trf1.jus.br>
2026-03-13 15:31:38 -0400

    RC-0.9.0

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  SIPE_WEB_URL: 'http://172.29.4.26',
-  SIPE_API_URL: 'http://172.29.4.26:8084',
-  SIPE_AUTH_URL: 'http://172.29.4.26:9000',
+  SIPE_WEB_URL: 'http://srvsipe-rr.rr.trf1.gov.br',
+  SIPE_API_URL: 'http://srvsipe-rr.rr.trf1.gov.br:8084',
+  SIPE_AUTH_URL: 'http://srvsipe-rr.rr.trf1.gov.br:9000',
   SIPE_LOTACAO_PAI: 15
 };
